7aaf3a8d5b1067cc5fd82d84654627df3040f528
Krzysztof Michalski <krzysztofmichalski42@gmail.com>
2018-06-25 21:27:34 +0200

    Added support for donwloading from series page and code refactoring

--- download.php
+++ download.php
@@ -12,41 +12,45 @@
  * php downloader.php [URLs] {options}
  *
  * Options:
- * --no-convert - prevents packing-up to .cbz archive
- * --keep-files - prevents automatic deletion of downloaded files
- * --till-last-chapter - continues to download next chapters until the end of a current series
- * --skip-download - skips downloading files (usefull in debugging)
+ * --no-convert, -n - prevents packing-up to .cbz archive
+ * --keep-files, -k - prevents automatic deletion of downloaded files
+ * --untill-last-chapter, -l - continues to download next chapters until the end of a current series
+ * --skip-download, -s - skips downloading files (usefull in debugging)
 */
-
-//Global variables
-$tillLastChapter = false;
-$keepFiles = false;
-$convert = true;
-$downloadQueue = array();
-$skipDownload = false;
 
 function consoleLog($message) {
   echo $message."\n";
 }
 
-function setupProgram() {
-  global $argc, $argv, $downloadQueue, $keepFiles, $tillLastChapter, $convert, $skipDownload;
+function setDeafults(&$options) {
+  $options["untillLastChapter"] = false;
+  $options["keepFiles"] = false;
+  $options["convert"] = true;
+  $options["skipDownload"] = false;
+}
+
+//Checking passed arguments
+function setupProgram(&$options, &$downloadQueue) {
+  global $argc, $argv;
   for ($i=1;$i<$argc;$i++) {
-    if (substr($argv[$i], 0 , 2) == "--") {
+    if (substr($argv[$i], 0 , 1) == "-") {
       switch ($argv[$i]) {
         case '--keep-files':
-          $keepFiles = true;
+        case '-k':
+          $options["keepFiles"] = true;
           break;
-        case '--till-last-chapter':
-          $tillLastChapter = true;
+        case '--untill-last-chapter':
+        case '-l':
+          $options["untillLastChapter"] = true;
           break;
         case '--no-convert':
-          $convert = false;
+        case '-n':
+          $options["convert"] = false;
           break;
         case '--skip-download':
-          $skipDownload = true;
+        case '-s':
+          $options["skipDownload"] = true;
           break;
-
         default:
           consoleLog("Unknown option: $argv[$i]");
           die();
@@ -57,6 +61,7 @@ function setupProgram() {
   }
 }
 
+//Sets up CURL for downloading webpage
 function setupCURL(&$ch) {
   curl_setopt($ch, CURLOPT_TIMEOUT, 20);
   curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -66,6 +71,7 @@ function setupCURL(&$ch) {
   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1");
 }
 
+//Extracts data (iamges, title) from HTML
 function getData(&$html, &$json, &$dirName) {
   //Getting images
   consoleLog("Getting JSON data...");
@@ -80,6 +86,7 @@ function getData(&$html, &$json, &$dirName) {
   $dirName = substr($html, $posNameStart+16, $posNameEnd-$posNameStart-16);
 }
 
+//Downloads image, creates filename
 function downloadImage(&$key, &$dirName, &$count, &$imageCount) {
   consoleLog("Downloading file ".($count+1)."/".$imageCount);
   $key = trim($key);
@@ -101,6 +108,7 @@ function downloadImage(&$key, &$dirName, &$count, &$imageCount) {
   $count++;
 }
 
+//Adds files to CBZ archive and outputs it to $dirName.cbz
 function convertToCBZ($dirName) {
   consoleLog("Creating archive...");
   $zip = new ZipArchive();
@@ -116,6 +124,7 @@ function convertToCBZ($dirName) {
   $zip->close();
 }
 
+//Cleans up downloaded files, skippable with --keep-files
 function removeFiles($dirName) {
   //Getting files to add
   $fileTable = scandir($dirName);
@@ -126,22 +135,30 @@ function removeFiles($dirName) {
   rmdir($dirName);
 }
 
+//Gets line with id of next chapter
 function getNextIdLine(&$html) {
   $chapterIdStart = strpos($html, 'var nextCha =');
   $chapterIdEnd = strpos($html, ';', $chapterIdStart);
   return substr($html, $chapterIdStart, $chapterIdEnd-$chapterIdStart);
 }
 
+//Gets id of next chapter
 function getUniqueId($chapterId) {
   $chapterId = substr($chapterId, 14);
   $chapterId = json_decode($chapterId);
   return $chapterId->base->uniqueId;
 }
 
-function downloadChapter($url) {
-  global $keepFiles, $tillLastChapter, $convert, $skipDownload;
-  //Downloaing page
-  $json = $dirName = "";
+//Checks if given URL is linking to series or chapter
+function checkURL(&$url) {
+  if (strpos($url, "/series/")) {
+    return true;
+  }
+  return false;
+}
+
+//Downloads webpage to $html
+function downloadWebpage($url) {
   $ch = curl_init($url);
   setupCURL($ch);
   consoleLog("Downloading webpage $url");
@@ -152,18 +169,47 @@ function downloadChapter($url) {
   }
   $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
   curl_close($ch);
+  return $html;
+}
 
+function getChapterURL($id) {
+  return "https://bato.to/chapter/".$id;
+}
+
+//Returns URL to first chapter of series
+function getFirstChapterURL($html) {
+  $url = substr($html, strrpos($html, '<a class="chapt" href="/chapter/'));
+  $matches;
+  preg_match('/\/chapter\/[0-9]{1,}/', $url, $matches);
+  $id = substr($matches[0], strrpos($matches[0], "/")+1);
+  return getChapterURL($id);
+}
+
+function downloadChapter($url, &$options) {
+  //Downloaing page
+  $isSeries = checkURL($url);
+  $json = $dirName = $html = "";
+  $html = downloadWebpage($url);
+  if ($isSeries) {
+    consoleLog("Given URL is a link to series page, downloading first chapter instead.");
+    $url = getFirstChapterURL($html);
+    $html = downloadWebpage($url);
+  }
   //Getting images URLs
   if ($html !== false) {
     //Decoding JSON and making directory
     getData($html, $json, $dirName);
+    //Sanitize file name
+    $dirName = preg_replace( '/[^a-zA-Z0-9]+/', '_', $dirName);
+    //Change JSON to array
     $file = json_decode($json, true);
     $count = 0;
     $imageCount = count($file);
+    //Create directory
     if (!is_dir($dirName)) {
       mkdir($dirName);
     }
-    if (!$skipDownload) {
+    if (!$options["skipDownload"]) {
       //Loop for downloading images
       consoleLog("Downloading images for \"$dirName\"");
       foreach ($file as $key) {
@@ -171,19 +217,21 @@ function downloadChapter($url) {
       }
     }
     //Creating archive
-    if ($convert) {
+    if ($options["convert"]) {
       convertToCBZ($dirName);
     } else {
       consoleLog("Skipping conversion to .cbz archive");
     }
-    if (!$keepFiles) {
+    //Removing files
+    if (!$options["keepFiles"]) {
       removeFiles($dirName);
     }
     consoleLog("Done");
-    if ($tillLastChapter) {
+    //Get next chapter if user wants to
+    if ($options["untillLastChapter"]) {
       $chapterLine = getNextIdLine($html);
       if (strpos($chapterLine, "= null") === false) {
-        downloadChapter("https://bato.to/chapter/".getUniqueId($chapterLine));
+        downloadChapter(getChapterURL(getUniqueId($chapterLine)), $options);
       } else {
         consoleLog("Reached last chapter, stopping program.");
       }
@@ -191,14 +239,19 @@ function downloadChapter($url) {
   }
 }
 
-//Main program
-
-setupProgram();
-foreach ($downloadQueue as $downloadItem) {
-  downloadChapter($downloadItem);
+//Main program startup
+function main() {
+  //Setting up initial values
+  $options = $downloadQueue = [];
+  //Sets up default and user options
+  setDeafults($options);
+  setupProgram($options, $downloadQueue);
+  //Main download loop
+  foreach ($downloadQueue as $downloadItem) {
+    downloadChapter($downloadItem, $options);
+  }
 }
 
-
-
+main();
 
  ?>
